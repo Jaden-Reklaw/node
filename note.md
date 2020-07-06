f0f6f62f9f6afc7a61181a21e0f94dd6941b11d8
Jordan Walker <jwalker117@gmail.com>
2020-07-06 10:20:05 -0500

    added middleware for json and to access req.body

---
+++ note.md
@@ -0,0 +1,20 @@
+RESTful API
+
+Set of functions to get request and responses over HTTP.
+
+GET: Get a resource
+POST: Add a resource
+PUT: Update a resource
+DELETE: Remove a resource
+
+Url:
+/profile
+
+get a profile
+post a new profile
+delete a profile
+update a profile
+
+
+REST API are stateless
+calls can be made independently of one another and each call contains all the necessary data to complete itself successfully.
--- server2.js
+++ server2.js
@@ -4,11 +4,16 @@ const express = require('express');
 //Assign express function to app to use its functions
 const app = express();
 
+//in order to access req.body you need the following line
+app.use(express.urlencoded({extended: false}));
+//in order for the server to recieve json from the front end
+app.use(express.json());
+
 //Add middleware to the express server
-app.use((req, res, next) => {
-    console.log('<h1>Hello World!</h1>');
-    next();
-})
+// app.use((req, res, next) => {
+//     console.log('<h1>Hello World!</h1>');
+//     next();
+// })
 
 //Methods to use with server get, post, delete and put
 //Example of get routes you can also use post, put, and delete
@@ -34,12 +39,11 @@ app.get('/bill', (req, res) => {
 
 //Even though you have a post and a get express allows you to use the same
 //route and still do different things.
-app.post('/bill', (req, res) => {
-    const user = {
-        name: 'Sally',
-        hobby: 'basketball'
-    }
-    res.send(user);
+app.post('/profile', (req, res) => {
+    console.log('req.body is',req.body);
+    res.sendStatus(201);
 });
 
 app.listen(3000);
+
+
